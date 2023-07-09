d13caaa0fa87ba57d48c63e1e360a15e07e7e0d0
Josh Holmer <jholmer.in@gmail.com>
2023-07-09 13:32:32 -0400

    Revert "Provide BufferedZopfliDeflater and allow user to pass in a custom Deflater (#530)"
    This reverts commit 2a59419bdfe0d8d5be3f9bbd316f82bb33492675.

--- benches/zopfli.rs
+++ benches/zopfli.rs
@@ -5,6 +5,7 @@ extern crate test;
 
 use oxipng::internal_tests::*;
 use oxipng::*;
+use std::num::NonZeroU8;
 use std::path::PathBuf;
 use test::Bencher;
 
@@ -14,7 +15,6 @@ const DEFAULT_DEFLATER: BufferedZopfliDeflater = BufferedZopfliDeflater::const_d
 fn zopfli_16_bits_strategy_0(b: &mut Bencher) {
     let input = test::black_box(PathBuf::from("tests/files/rgb_16_should_be_rgb_16.png"));
     let png = PngData::new(&input, &Options::default()).unwrap();
-    let max_size = AtomicMin::new(Some(png.idat_data.len()));
 
     b.iter(|| {
         DEFAULT_DEFLATER
@@ -27,7 +27,6 @@ fn zopfli_16_bits_strategy_0(b: &mut Bencher) {
 fn zopfli_8_bits_strategy_0(b: &mut Bencher) {
     let input = test::black_box(PathBuf::from("tests/files/rgb_8_should_be_rgb_8.png"));
     let png = PngData::new(&input, &Options::default()).unwrap();
-    let max_size = AtomicMin::new(Some(png.idat_data.len()));
 
     b.iter(|| {
         DEFAULT_DEFLATER
@@ -42,7 +41,6 @@ fn zopfli_4_bits_strategy_0(b: &mut Bencher) {
         "tests/files/palette_4_should_be_palette_4.png",
     ));
     let png = PngData::new(&input, &Options::default()).unwrap();
-    let max_size = AtomicMin::new(Some(png.idat_data.len()));
 
     b.iter(|| {
         DEFAULT_DEFLATER
@@ -57,7 +55,6 @@ fn zopfli_2_bits_strategy_0(b: &mut Bencher) {
         "tests/files/palette_2_should_be_palette_2.png",
     ));
     let png = PngData::new(&input, &Options::default()).unwrap();
-    let max_size = AtomicMin::new(Some(png.idat_data.len()));
 
     b.iter(|| {
         DEFAULT_DEFLATER
@@ -72,7 +69,6 @@ fn zopfli_1_bits_strategy_0(b: &mut Bencher) {
         "tests/files/palette_1_should_be_palette_1.png",
     ));
     let png = PngData::new(&input, &Options::default()).unwrap();
-    let max_size = AtomicMin::new(Some(png.idat_data.len()));
 
     b.iter(|| {
         DEFAULT_DEFLATER
